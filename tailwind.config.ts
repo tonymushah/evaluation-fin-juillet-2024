c2cd01bd0cc943588a8701b561f19962a451aae1
tonymushah <tonyramanantsalama@gmail.com>
2024-07-26 10:26:38 +0300

    added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,39 @@
-import flowbitePlugin from 'flowbite/plugin'
+import flowbitePlugin from 'flowbite/plugin';
 import typo from '@tailwindcss/typography';
 
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 
 	theme: {
 		extend: {
-      colors: {
-        // flowbite-svelte
-        primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B'
-        }
-      }
-    }
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#FFF5F2',
+					100: '#FFF1EE',
+					200: '#FFE4DE',
+					300: '#FFD5CC',
+					400: '#FFBCAD',
+					500: '#FE795D',
+					600: '#EF562F',
+					700: '#EB4F27',
+					800: '#CC4522',
+					900: '#A5371B'
+				}
+			}
+		}
 	},
 
-	plugins: [flowbitePlugin, typo()]
+	plugins: [flowbitePlugin, typo()],
+	fontFamily: {
+		mono: ["'Fira Mono'", 'monospace'],
+		serif: ["'Inter Variable'", 'sans-serif'],
+		sans: ["'Poppins'", 'sans-serif'],
+		brand: ["'Freckle Face'", 'system-ui']
+	}
 } as Config;
