72ac2160f5715ed65b43759b51e1d3e193dd9b1e
tonymushah <tonyramanantsalama@gmail.com>
2024-07-26 18:28:07 +0300

    Added build rs

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let default_conf = tonic_build::configure().build_client(false);
+    default_conf
+        .clone()
+        .compile(&["./protos/admin.proto"], &["./protos"])
+        .unwrap();
+    default_conf
+        .clone()
+        .compile(&["./protos/client.proto"], &["./protos"])
+        .unwrap();
+}
