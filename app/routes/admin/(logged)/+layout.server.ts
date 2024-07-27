1be8c158acf7dc5d85902bf287091bdc700abe6e
tonymushah <tonyramanantsalama@gmail.com>
2024-07-27 07:32:25 +0300

    Added ssr for admin `+layout.server.ts`

--- app/routes/admin/(logged)/+layout.server.ts
+++ app/routes/admin/(logged)/+layout.server.ts
@@ -2,6 +2,8 @@ import { ADMIN_BACK_URL } from '$env/static/private';
 import { getToken } from '$lib/admin/token.server';
 import type { LayoutServerLoad } from './$types';
 
+export const ssr = true;
+
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	return {
 		//token: getToken(cookies),
