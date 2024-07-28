39353d0582a91f0ce1a88b31c1d5aca07d707171
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 21:38:50 +0300

    Added `commonDateToDate` and `dateToCommonDate`

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -1,1 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { Date as CommonDate } from './protos/commons';
+
+export function commonDateToDate(date: CommonDate) {
+	return new Date(date.annee, date.mois, date.jour);
+}
+
+export function dateToCommonDate(date: Date): CommonDate {
+	return {
+		jour: date.getDay(),
+		mois: date.getMonth(),
+		annee: date.getFullYear()
+	};
+}
