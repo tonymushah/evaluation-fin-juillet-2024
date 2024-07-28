4d0b4c3783cf87ca0f4c8fdc871dcf3d2f1967d8
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 14:55:48 +0300

    Added `HelloServ` for client

---
+++ src/servers/client/hello.rs
@@ -0,0 +1,21 @@
+use proto_client::{hello_service_server::HelloService, HelloRequest, HelloResponse};
+use tonic::{Request, Response};
+
+use crate::servers::TonicRpcResult;
+
+#[derive(Debug, Clone, Copy)]
+pub struct HelloServ;
+
+#[tonic::async_trait]
+impl HelloService for HelloServ {
+    async fn say(&self, request: Request<HelloRequest>) -> TonicRpcResult<HelloResponse> {
+        let content = &request.get_ref().content;
+        Ok(Response::new(HelloResponse {
+            message: if content.len() > 10 {
+                "Sorry! You said something more than 10 characters".into()
+            } else {
+                format!("Hi! It's the server! You said {content}.")
+            },
+        }))
+    }
+}
