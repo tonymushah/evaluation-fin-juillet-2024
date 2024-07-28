818fcafcd4de93c871e2ed7cf82d8baa1b69cfff
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 15:05:52 +0300

    Added `AuthService` for client

--- src/servers/client.rs
+++ src/servers/client.rs
@@ -1,3 +1,5 @@
+pub mod auth;
 pub mod hello;
 
+pub use auth::AuthService;
 pub use hello::HelloServ;
---
+++ src/servers/client/auth.rs
@@ -0,0 +1,16 @@
+use proto_client::{auth_server::Auth, LoginRequest, LoginResponse};
+use tonic::Request;
+
+use crate::{servers::TonicRpcResult, tonic_not_implemented, DbPool};
+
+#[derive(Debug, Clone)]
+pub struct AuthService {
+    pub pool: DbPool,
+}
+
+#[tonic::async_trait]
+impl Auth for AuthService {
+    async fn login(&self, _reuest: Request<LoginRequest>) -> TonicRpcResult<LoginResponse> {
+        tonic_not_implemented()
+    }
+}
