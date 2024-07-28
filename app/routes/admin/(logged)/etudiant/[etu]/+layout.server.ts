4d1e9fd6fbb6745dae5609f7447b1077ec13e0b3
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 23:00:03 +0300

    Some faker data updates

--- app/routes/admin/(logged)/etudiant/[etu]/+layout.server.ts
+++ app/routes/admin/(logged)/etudiant/[etu]/+layout.server.ts
@@ -1,20 +1,19 @@
+import { dateToCommonDate } from '$lib';
 import type { Etudiant } from '$lib/protos/commons';
+import dayjs from 'dayjs';
 import type { LayoutServerLoad } from './$types';
 import { faker } from '@faker-js/faker';
 
 export const ssr = true;
 
 export const load: LayoutServerLoad = async function () {
+	const birthday = faker.date.birthdate();
 	const etudiant: Etudiant = {
 		nom: faker.person.lastName(),
 		prenom: faker.person.firstName(),
-		dateNaissance: {
-			annee: 2000,
-			jour: 12,
-			mois: 1
-		},
+		dateNaissance: dateToCommonDate(birthday),
 		numero: 'Numero etu',
-		age: 24,
+		age: dayjs(new Date()).diff(birthday, 'years'),
 		promotion: 'P15'
 	};
 	return {
