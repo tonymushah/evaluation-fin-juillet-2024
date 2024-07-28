e3fa07d92f7ace7bece588849dd3914cdde483c1
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 23:06:57 +0300

    Added `+layout.svelte`

---
+++ app/routes/admin/(logged)/etudiant/[etu]/[semestre]/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button, P } from 'flowbite-svelte';
+	import { ArrowLeftOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Button color="purple" on:click={() => history.back()}>
+	<div class="flex flex-row flex-nowrap items-center gap-2">
+		<ArrowLeftOutline />
+		<P>Back to lists</P>
+	</div>
+</Button>
+
+<slot />
