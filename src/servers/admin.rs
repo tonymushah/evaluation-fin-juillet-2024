3cc34bc70b1a1e951e53b8e80bb3ae96a0054937
tonymushah <tonyramanantsalama@gmail.com>
2024-07-28 14:43:36 +0300

    Added `AuthService` for admin

--- src/servers/admin.rs
+++ src/servers/admin.rs
@@ -1,9 +1,11 @@
+pub mod auth;
 pub mod database;
 pub mod etudiants;
 pub mod getters;
 pub mod hello;
 pub mod notes;
 
+pub use auth::AuthService;
 pub use database::DatabaseService;
 pub use etudiants::EtudiantsService;
 pub use getters::GettersService;
