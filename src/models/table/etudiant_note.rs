b6893ab75f7dcdbc55d0f0a274448e3e87664ecc
tonymushah <tonyramanantsalama@gmail.com>
2024-07-30 01:03:54 +0300

    Added `EtudiantNotes`

--- src/models/table/etudiant_note.rs
+++ src/models/table/etudiant_note.rs
@@ -1,4 +1,6 @@
-use bigdecimal::BigDecimal;
+use std::cmp::Ordering;
+
+use bigdecimal::ToPrimitive;
 use diesel::{prelude::*, QueryResult};
 use diesel_schemas::view::v_etudiant_matiere_note;
 use time::{OffsetDateTime, PrimitiveDateTime};
@@ -13,10 +15,10 @@ pub struct GetReleveNote {
     pub semestre: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct EtudiantNoteUnit {
     matiere: String,
-    note: BigDecimal,
+    note: f64,
 }
 
 #[derive(Debug, Clone)]
@@ -25,8 +27,25 @@ pub enum EtudiantNote {
     Choix(Vec<EtudiantNoteUnit>),
 }
 
+impl EtudiantNote {
+    pub fn unit(&self) -> Option<EtudiantNoteUnit> {
+        match self {
+            EtudiantNote::Unique(u) => Some(u.clone()),
+            EtudiantNote::Choix(c) => c
+                .iter()
+                .max_by(|a, b| a.note.partial_cmp(&b.note).unwrap_or(Ordering::Equal))
+                .cloned(),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct EtudiantNotes(pub Vec<EtudiantNote>);
+
+fn now() -> PrimitiveDateTime {
+    let now = OffsetDateTime::now_utc();
+    PrimitiveDateTime::new(now.date(), now.time())
+}
 
 impl GetReleveNote {
     pub fn get_semestre_matiere(&self, con: &mut PgConnection) -> QueryResult<SemestreMatieres> {
@@ -37,12 +56,16 @@ impl GetReleveNote {
         v_etudiant_matiere_note
             .filter(etu.eq(&self.etudiant))
             .filter(semestre.eq(&self.semestre))
-            .filter(submission.le({
-                let now = OffsetDateTime::now_utc();
-                PrimitiveDateTime::new(now.date(), now.time())
-            }))
+            .filter(submission.le(now()))
             .select(VEtudiantMatiereNote::as_select())
             .load(con)
+    }
+    pub fn get(&self, con: &mut PgConnection) -> QueryResult<EtudiantNotes> {
+        let vnotes = self.get_v_notes(con)?;
+        let matieres = self.get_semestre_matiere(con)?;
+        let mut notes: EtudiantNotes = (&matieres).into();
+        notes.seed(&vnotes);
+        Ok(notes)
     }
 }
 
@@ -67,16 +90,24 @@ impl EtudiantNotes {
             if let Some(value) = notes.iter().max_by(|a, b| a.submission.cmp(&b.submission)) {
                 match note {
                     EtudiantNote::Unique(e) => {
-                        e.note = value.valeur.clone();
+                        e.note = value.valeur.to_f64().unwrap_or_default();
                     }
                     EtudiantNote::Choix(e) => {
                         if let Some(v) = e.iter_mut().find(|e| e.matiere == value.matiere) {
-                            v.note = value.valeur.clone();
+                            v.note = value.valeur.to_f64().unwrap_or_default();
                         }
                     }
                 }
             }
         });
+    }
+    pub fn moyenne(&self) -> f64 {
+        let units = self.0.iter().flat_map(|m| m.unit()).collect::<Vec<_>>();
+        if units.is_empty() {
+            0f64
+        } else {
+            units.iter().map(|u| u.note).sum::<f64>() / units.len() as f64
+        }
     }
 }
 
@@ -90,19 +121,99 @@ impl From<&SemestreMatieres> for EtudiantNotes {
                     super::sem_mat::SemestreMatiere::Unique(mat) => {
                         EtudiantNote::Unique(EtudiantNoteUnit {
                             matiere: mat.id_matiere.clone(),
-                            note: BigDecimal::default(),
+                            note: 0f64,
                         })
                     }
                     super::sem_mat::SemestreMatiere::Choix(mats) => EtudiantNote::Choix(
                         mats.iter()
                             .map(|mat| EtudiantNoteUnit {
                                 matiere: mat.id_matiere.clone(),
-                                note: BigDecimal::default(),
+                                note: 0f64,
                             })
                             .collect(),
                     ),
                 })
                 .collect(),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bigdecimal::{BigDecimal, FromPrimitive};
+    use diesel::{insert_into, prelude::*};
+    use time::Date;
+    use uuid::Uuid;
+
+    use crate::models::table::{Etudiant, Note, Promotion};
+    use diesel_schemas::schema::{etudiant, note, promotion};
+
+    use super::{now, GetReleveNote};
+
+    fn seed(con: &mut PgConnection) -> QueryResult<()> {
+        let prom = Promotion {
+            id_promotion: "P15".into(),
+            nom: None,
+        };
+        let etudiant_ = Etudiant {
+            etu: "ETU001844".into(),
+            nom: "RAMANANTSALAMA".into(),
+            prenom: "Dirk Tony".into(),
+            date_naissance: Date::from_calendar_date(2006, time::Month::April, 6).unwrap(),
+            promotion: prom.id_promotion.clone(),
+            genre: 1,
+        };
+        let notes = vec![
+            Note {
+                id_note: Uuid::new_v4(),
+                etudiant: etudiant_.etu.clone(),
+                matiere: "INF304".into(),
+                submission: now(),
+                valeur: BigDecimal::from_f64(3.9).unwrap_or_default(),
+            },
+            Note {
+                id_note: Uuid::new_v4(),
+                etudiant: etudiant_.etu.clone(),
+                matiere: "INF301".into(),
+                submission: now(),
+                valeur: BigDecimal::from_f64(10.0).unwrap_or_default(),
+            },
+            Note {
+                id_note: Uuid::new_v4(),
+                etudiant: etudiant_.etu.clone(),
+                matiere: "ORG303".into(),
+                submission: now(),
+                valeur: BigDecimal::from_f64(12.82).unwrap_or_default(),
+            },
+            Note {
+                id_note: Uuid::new_v4(),
+                etudiant: etudiant_.etu.clone(),
+                matiere: "INF307".into(),
+                submission: now(),
+                valeur: BigDecimal::from_f64(10f64).unwrap_or_default(),
+            },
+        ];
+        insert_into(promotion::dsl::promotion)
+            .values(prom)
+            .execute(con)?;
+        insert_into(etudiant::dsl::etudiant)
+            .values(etudiant_)
+            .execute(con)?;
+        insert_into(note::dsl::note).values(notes).execute(con)?;
+        Ok(())
+    }
+    #[test]
+    fn test_data() -> anyhow::Result<()> {
+        let pool = crate::etablish_connection();
+        let mut con = pool.get()?;
+        let _ = con.transaction(|con| seed(con));
+        let data = GetReleveNote {
+            etudiant: "ETU001844".into(),
+            semestre: "S5".into(),
+        }
+        .get(&mut con)?;
+        assert_eq!(data.0.len(), 6);
+        println!("{}", data.moyenne());
+        Ok(())
     }
 }
