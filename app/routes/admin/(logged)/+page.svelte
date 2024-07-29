c97b7fa8b1b066127a7624e50ac5b687e4c677fa
tonymushah <tonyramanantsalama@gmail.com>
2024-07-29 08:53:25 +0300

    Added "basic" admin dashboard

---
+++ app/routes/admin/(logged)/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { route } from '$lib/ROUTES';
+
+	import { Button, Heading, Hr, P } from 'flowbite-svelte';
+	let admis = 12;
+	let ajournee = 16;
+</script>
+
+<div class="mx-10">
+	<section>
+		<Heading tag="h2">Statistique etudiant</Heading>
+		<P>Admis en licence: {admis}</P>
+		<P>Ajourees: {ajournee}</P>
+	</section>
+	<Hr />
+	<section>
+		<Heading class="mb-3" tag="h2">Insertions</Heading>
+		<div>
+			<Button on:click={() => goto(route('/admin/note-insert'))}>Insertion des notes</Button>
+		</div>
+	</section>
+</div>
