def87b8f52cd117d9c59c40da4e9689246f87131
tonymushah <tonyramanantsalama@gmail.com>
2024-07-30 02:32:42 +0300

    implemented client login

--- src/servers/client/auth.rs
+++ src/servers/client/auth.rs
@@ -1,7 +1,9 @@
+use diesel::prelude::*;
+use diesel_schemas::schema::etudiant;
 use proto_client::{auth_server::Auth, LoginRequest, LoginResponse};
-use tonic::Request;
+use tonic::{Request, Response};
 
-use crate::{servers::TonicRpcResult, tonic_not_implemented, DbPool};
+use crate::{servers::TonicRpcResult, DbPool};
 
 #[derive(Debug, Clone)]
 pub struct AuthService {
@@ -10,7 +12,20 @@ pub struct AuthService {
 
 #[tonic::async_trait]
 impl Auth for AuthService {
-    async fn login(&self, _reuest: Request<LoginRequest>) -> TonicRpcResult<LoginResponse> {
-        tonic_not_implemented()
+    async fn login(&self, request: Request<LoginRequest>) -> TonicRpcResult<LoginResponse> {
+        let pool = self.pool.clone();
+        let numero = request.get_ref().numero.clone();
+
+        let token = crate::spawn_blocking(move || -> crate::Result<String> {
+            use self::etudiant::dsl::*;
+            let mut con = pool.get()?;
+            let res = etudiant
+                .filter(etu.eq(numero.clone()))
+                .select(etu)
+                .get_result::<String>(&mut con);
+            Ok(res?)
+        })
+        .await??;
+        Ok(Response::new(LoginResponse { token }))
     }
 }
