099c0cdda62eb1d78f16c00560c967a075a07271
tonymushah <tonyramanantsalama@gmail.com>
2024-07-30 05:04:03 +0300

    implemented `GettersService` rpc methodes

--- src/servers/admin/getters.rs
+++ src/servers/admin/getters.rs
@@ -1,11 +1,14 @@
 use proto_admin::{
     getters_server::Getters, GetMatieresRequest, GetMatieresResponse, GetPromotionsRequest,
-    GetPromotionsResponse, GetSemetresRequest, GetSemetresResponse,
+    GetPromotionsResponse, GetSemetresRequest, GetSemetresResponse, Semestre,
 };
 
-use tonic::Request;
+use diesel::prelude::*;
+use diesel_schemas::schema::{promotion, semestre};
 
-use crate::{servers::TonicRpcResult, DbPool};
+use tonic::{Request, Response};
+
+use crate::{models::table::Matiere, paginate::Paginate, servers::TonicRpcResult, DbPool};
 
 #[derive(Debug, Clone)]
 pub struct GettersService {
@@ -16,20 +19,66 @@ pub struct GettersService {
 impl Getters for GettersService {
     async fn semetres(
         &self,
-        request: Request<GetSemetresRequest>,
+        _request: Request<GetSemetresRequest>,
     ) -> TonicRpcResult<GetSemetresResponse> {
-        crate::tonic_not_implemented()
+        let pool = self.pool.clone();
+        let sems = crate::spawn_blocking(move || -> crate::Result<Vec<Semestre>> {
+            use self::semestre::dsl::*;
+            let mut con = pool.get()?;
+            Ok(semestre
+                .select(id_sem)
+                .get_results::<String>(&mut con)?
+                .into_iter()
+                .map(|sem| Semestre { numero: sem })
+                .collect())
+        })
+        .await??;
+        Ok(Response::new(GetSemetresResponse { semestre: sems }))
     }
     async fn matieres(
         &self,
         request: Request<GetMatieresRequest>,
     ) -> TonicRpcResult<GetMatieresResponse> {
-        crate::tonic_not_implemented()
+        let pool = self.pool.clone();
+        let req_data = request.get_ref().clone();
+        let req = req_data.clone();
+        let (data, total) = crate::spawn_blocking(move || -> crate::Result<(Vec<Matiere>, i64)> {
+            use diesel::prelude::*;
+            use diesel_schemas::schema::matiere::dsl::*;
+            let mut con = pool.get()?;
+            let offset = req.offset.unwrap_or_default();
+            let limit = req.limit.unwrap_or(10);
+            let mut query = matiere.into_boxed();
+            if !req.semstres.is_empty() {
+                query = query.filter(semestre.eq_any(req.semstres));
+            }
+            Ok(query
+                .paginate(offset as i64, limit as i64)
+                .load_data(&mut con)?)
+        })
+        .await??;
+        let offset = req_data.offset.unwrap_or_default();
+        let limit = req_data.limit.unwrap_or(10);
+        Ok(Response::new(GetMatieresResponse {
+            matieres: data.into_iter().map(|e| e.into()).collect(),
+            offset,
+            limit,
+            total: total as u64,
+        }))
     }
     async fn promotions(
         &self,
-        request: Request<GetPromotionsRequest>,
+        _request: Request<GetPromotionsRequest>,
     ) -> TonicRpcResult<GetPromotionsResponse> {
-        crate::tonic_not_implemented()
+        let pool = self.pool.clone();
+        let proms = crate::spawn_blocking(move || -> crate::Result<Vec<String>> {
+            use self::promotion::dsl::*;
+            let mut con = pool.get()?;
+            Ok(promotion
+                .select(id_promotion)
+                .get_results::<String>(&mut con)?)
+        })
+        .await??;
+        Ok(Response::new(GetPromotionsResponse { promotions: proms }))
     }
 }
