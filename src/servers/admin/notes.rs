fb7bdbdf6f9125530c40e1845602dcb810a9c14b
tonymushah <tonyramanantsalama@gmail.com>
2024-07-30 05:59:08 +0300

    implemeted `NotesService::insert`

--- src/servers/admin/notes.rs
+++ src/servers/admin/notes.rs
@@ -1,10 +1,17 @@
 use std::pin::Pin;
 
+use bigdecimal::{BigDecimal, FromPrimitive};
+use diesel::{insert_into, prelude::*};
 use proto_admin::{notes_server::Notes, InsertNotesRequest, InsertNotesResponse};
-use tokio_stream::Stream;
-use tonic::{Request, Streaming};
+use tokio_stream::{Stream, StreamExt};
+use tonic::{Request, Response, Streaming};
+use uuid::Uuid;
 
-use crate::{servers::TonicRpcResult, tonic_not_implemented, DbPool};
+use crate::{
+    models::table::{etudiant_note::now, Note},
+    servers::TonicRpcResult,
+    DbPool,
+};
 
 #[derive(Debug, Clone)]
 pub struct NotesService {
@@ -20,6 +27,32 @@ impl Notes for NotesService {
         &self,
         request: Request<Streaming<InsertNotesRequest>>,
     ) -> TonicRpcResult<Self::InsertStream> {
-        tonic_not_implemented()
+        let pool = self.pool.clone();
+        let mut stream = request.into_inner();
+
+        let output = async_stream::try_stream! {
+            while let Some(input) = stream.next().await {
+                let input = input?;
+                let req_id = input.req_id.clone();
+                let custom_message = String::new();
+                let poll = pool.clone();
+                crate::spawn_blocking(move || -> crate::Result<Note> {
+                    use diesel_schemas::schema::note::dsl::*;
+                    let mut con = poll.get()?;
+                    Ok(insert_into(note).values(Note {
+                        id_note: Uuid::new_v4(),
+                        etudiant: input.etudiant,
+                        matiere: input.matiere,
+                        valeur: BigDecimal::from_f32(input.note as f32).unwrap_or_default(),
+                        submission: now()
+                    }).returning(Note::as_returning()).get_result(&mut con)?)
+                }).await??;
+                yield InsertNotesResponse {
+                    req_id,
+                    custom_message
+                };
+            }
+        };
+        Ok(Response::new(Box::pin(output)))
     }
 }
