957661b4971657718143e61fb8ff9936c3e67d0e
tonymushah <tonyramanantsalama@gmail.com>
2024-09-01 13:19:44 +0300

    fixed imports

--- app/routes/admin/(logged)/config/+page.server.ts
+++ app/routes/admin/(logged)/config/+page.server.ts
@@ -1,8 +1,8 @@
-import { GetConfigsMessageResponse } from '$lib/protos/admin';
+import { ConfigNote, GetConfigsMessageResponse } from '$lib/protos/admin';
 import { CfgNoteServiceClient } from '$lib/protos/admin.client';
 import { Empty } from '$lib/protos/commons';
 import { adminClient } from '$lib/server/protoclients';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const cfg_client = new CfgNoteServiceClient(adminClient);
@@ -13,3 +13,53 @@ export const load: PageServerLoad = async () => {
 		list: res.configs
 	};
 };
+
+export const actions = {
+	async default({ request }) {
+		const formData = await request.formData();
+		let code;
+
+		const form_code = formData.get('code');
+		if (typeof form_code == 'string') {
+			code = form_code;
+		} else {
+			return {
+				errors: 'Invalid code input'
+			};
+		}
+		let nom;
+		const form_nom = formData.get('nom');
+		if (typeof form_nom == 'string') {
+			nom = form_nom;
+		} else {
+			return {
+				code,
+				errors: 'Invalid nom input'
+			};
+		}
+		let valeur;
+		const form_valeur = formData.get('valeur');
+		if (typeof form_valeur == 'string') {
+			valeur = Number(form_valeur);
+			if (isNaN(valeur)) {
+				return {
+					code,
+					errors: 'Invalid valeur input'
+				};
+			}
+		} else {
+			return {
+				code,
+				errors: 'Invalid valeur input'
+			};
+		}
+		const toSend: ConfigNote = {
+			code,
+			name: nom,
+			value: valeur
+		};
+		console.log(toSend);
+		const cfg_client = new CfgNoteServiceClient(adminClient);
+		await cfg_client.updateConfig(ConfigNote.fromJson(toSend));
+	}
+} satisfies Actions;
--- app/routes/admin/(logged)/config/+page.svelte
+++ app/routes/admin/(logged)/config/+page.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
-	import { Heading, P } from 'flowbite-svelte';
-	import type { PageServerData } from './$types';
+	import { Button, Heading, Input, Label, Modal, P } from 'flowbite-svelte';
+	import type { ActionData, PageServerData } from './$types';
 	import ConfigsTable from '$lib/admin/components/config/ConfigsTable.svelte';
+	import { goto } from '$app/navigation';
+	import { route } from '$lib/ROUTES';
+	import type { ConfigNote } from '$lib/protos/admin';
+	import { derived, get, writable, type Writable } from 'svelte/store';
+	import { enhance } from '$app/forms';
 
 	export let data: PageServerData;
+
+	export let form: ActionData;
+	let inner_form: HTMLFormElement | undefined = undefined;
+	$: list_map = new Map(data.list.map((cfg) => [cfg.code, cfg]));
+	const selected = writable<ConfigNote | undefined>(undefined);
+	function select(code: string) {
+		selected.set(list_map.get(code));
+	}
+	const open_read = derived(selected, ($s) => $s != undefined);
+	const open: Writable<boolean> = {
+		subscribe(run, invalidate) {
+			return open_read.subscribe(run, invalidate);
+		},
+		set(value) {
+			if (value == false) {
+				selected.set(undefined);
+			}
+		},
+		update(updater) {
+			const value = updater(get(open_read));
+			if (value == false) {
+				selected.set(undefined);
+			}
+		}
+	};
 </script>
 
 <svelte:head>
@@ -14,4 +44,28 @@
 
 <P>Commancer a configurer les donnes des maintenant</P>
 
-<ConfigsTable data={data.list} />
+{#if form?.errors}
+	<P color="text-red-900 dark:text-red-300">{form.errors}</P>
+{/if}
+
+<ConfigsTable data={data.list} on:cellClick={(e) => select(e.detail.code)} />
+
+<Modal title="Metre a jour la configuration" bind:open={$open} autoclose outsideclose>
+	{#if $selected}
+		<form method="post" bind:this={inner_form} use:enhance class="grid gap-3">
+			<input hidden name="code" value={$selected.code} />
+			<div class="grid gap-1">
+				<Label class="text-xl">Nom</Label>
+				<Input name="nom" type="text" value={$selected.name} />
+			</div>
+			<div class="grid gap-1">
+				<Label class="text-xl">Valeur</Label>
+				<Input name="valeur" type="number" value={$selected.value} />
+			</div>
+		</form>
+	{/if}
+	<svelte:fragment slot="footer">
+		<Button on:click={() => inner_form?.submit()}>Update</Button>
+		<Button color="alternative">Cancel</Button>
+	</svelte:fragment>
+</Modal>
