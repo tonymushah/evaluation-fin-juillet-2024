ab07da4090d3ebef4e73eb35db89803394f80524
tonymushah <tonyramanantsalama@gmail.com>
2024-09-01 16:54:54 +0300

    Added `get_etudiants_status`

--- src/modules/etudiant_status.rs
+++ src/modules/etudiant_status.rs
@@ -1,4 +1,6 @@
-use diesel::{PgConnection, QueryResult};
+use std::collections::HashMap;
+
+use diesel::{prelude::*, PgConnection, QueryResult};
 use protos_commons::{EtudiantStatus, ReleveNoteStatus};
 
 use super::etudiant_note::get_etudiant_notes;
@@ -13,4 +15,16 @@ pub fn get_etudiant_status(etu: &str, con: &mut PgConnection) -> QueryResult<Etu
     } else {
         Ok(EtudiantStatus::EAdmis)
     }
+}
+
+pub fn get_etudiants_status(
+    con: &mut PgConnection,
+) -> QueryResult<HashMap<String, EtudiantStatus>> {
+    let etus: Vec<String> = {
+        use diesel_schemas::schema::etudiant::dsl::*;
+        etudiant.select(etu).get_results(con)?
+    };
+    etus.into_iter()
+        .map(|etu| -> QueryResult<_> { Ok((etu.clone(), get_etudiant_status(&etu, con)?)) })
+        .collect()
 }
