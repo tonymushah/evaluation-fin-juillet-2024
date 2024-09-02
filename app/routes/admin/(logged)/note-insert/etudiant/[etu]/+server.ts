987444dbdc0dd3414479ea2a71351054c3705273
tonymushah <tonyramanantsalama@gmail.com>
2024-09-02 07:38:30 +0300

    error handling update

--- app/routes/admin/(logged)/note-insert/etudiant/[etu]/+server.ts
+++ app/routes/admin/(logged)/note-insert/etudiant/[etu]/+server.ts
@@ -1,7 +1,7 @@
-import { json, error } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import type { InsertNotesRequest } from '$lib/protos/admin';
 import noteInsert from '$lib/admin/modules/noteInsert.server';
+import type { InsertNotesRequest } from '$lib/protos/admin';
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 
 type InsertValue = {
 	matiere?: string;
@@ -9,20 +9,21 @@ type InsertValue = {
 };
 export const POST: RequestHandler = async ({ params, request }) => {
 	const etudiant = params.etu;
-	const jsonBody: Record<string, InsertValue> = await request.json();
 	const to_send: InsertNotesRequest[] = [];
-	Object.entries(jsonBody).forEach(([id, value]) => {
-		if (value.matiere && value.valeur) {
-			to_send.push({
-				etudiant,
-				matiere: value.matiere,
-				note: value.valeur,
-				reqId: id
-			});
-		}
-	});
-	console.log(to_send);
 	try {
+		const jsonBody: Record<string, InsertValue> = await request.json();
+
+		Object.entries(jsonBody).forEach(([id, value]) => {
+			if (value.matiere && value.valeur) {
+				to_send.push({
+					etudiant,
+					matiere: value.matiere,
+					note: value.valeur,
+					reqId: id
+				});
+			}
+		});
+		console.log(to_send);
 		const result = await noteInsert(to_send);
 		return json(Object.fromEntries(result));
 	} catch (e) {
--- app/routes/admin/(logged)/note-insert/matiere/[mat]/+server.ts
+++ app/routes/admin/(logged)/note-insert/matiere/[mat]/+server.ts
@@ -8,21 +8,22 @@ type InsertValue = {
 	valeur?: number;
 };
 export const POST: RequestHandler = async ({ params, request }) => {
-	const matiere = params.mat;
-	const jsonBody: Record<string, InsertValue> = await request.json();
-	const to_send: InsertNotesRequest[] = [];
-	Object.entries(jsonBody).forEach(([id, value]) => {
-		if (value.etudiant && value.valeur) {
-			to_send.push({
-				etudiant: value.etudiant,
-				matiere,
-				note: value.valeur,
-				reqId: id
-			});
-		}
-	});
-	console.log(to_send);
 	try {
+		const matiere = params.mat;
+		const jsonBody: Record<string, InsertValue> = await request.json();
+		const to_send: InsertNotesRequest[] = [];
+		Object.entries(jsonBody).forEach(([id, value]) => {
+			if (value.etudiant && value.valeur) {
+				to_send.push({
+					etudiant: value.etudiant,
+					matiere,
+					note: value.valeur,
+					reqId: id
+				});
+			}
+		});
+		console.log(to_send);
+
 		const result = await noteInsert(to_send);
 		return json(Object.fromEntries(result));
 	} catch (e) {
